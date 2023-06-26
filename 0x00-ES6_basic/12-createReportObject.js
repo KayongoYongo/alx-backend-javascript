279f70ecde7408ed397d32b48f751145d38db417
KayongoYongo <sknsammie10@gmail.com>
2023-06-26 22:07:32 +0000

    create object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+
+  const reportObject = {
+    allEmployees,
+    getNumberOfDepartments() {
+      return Object.keys(allEmployees).length;
+    },
+  };
+
+  return reportObject;
+}
+
