705cd0769cde0de7108f4ccd83b360868cf93acb
KayongoYongo <sknsammie10@gmail.com>
2023-06-26 22:13:45 +0000

    create object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,13 @@
+export default function createIteratorObject(report) {
+  const departments = Object.values(report.allEmployees);
+
+  function* employeeIterator() {
+    for (const department of departments) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }
+
+  return employeeIterator();
+}
