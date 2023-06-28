0f8acd8ce9f737abd299fbe81d50752b3b8a9387
KayongoYongo <sknsammie10@gmail.com>
2023-06-28 23:30:37 +0000

    await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,22 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
