c0726954269204d4337b7ec98ef2ab64a82a61ea
KayongoYongo <sknsammie10@gmail.com>
2023-07-03 21:56:32 +0000

    create a list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,23 +1,10 @@
 const getListStudents = ()  => {
   let students = [
-    {
-      "id" : 1,
-      "firstName" : "Guillaume",
-      "location" : "San Francisco"
-    },
-    {
-      "id" : 2,
-      "firstName" : "James",
-      "location" : "Columbia"
-    },
-    {
-      "id" : 5,
-      "firstName" : "Serena",
-      "location" : "San Francisco"
-    }
+    { "id": 1, "firstName": "Guillaume", "location": "San Francisco" },
+    { "id": 2, "firstName": "James", "location": "Columbia" },
+    { "id": 5, "firstName": "Serena", "location": "San Francisco" }
   ];
-
-return students;
+  return students;
 };
 
 export default getListStudents;
