c8e6638ba86793cb7e2fd591c308d749f96e4a58
KayongoYongo <sknsammie10@gmail.com>
2023-07-03 22:01:43 +0000

    create a list

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,7 @@
+/**
+ * Retrieves a list of students.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 const getListStudents = ()  => {
   let students = [
     { "id": 1, "firstName": "Guillaume", "location": "San Francisco" },
