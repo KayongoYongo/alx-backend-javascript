3e1da70662ca3a8e20abd4dae7adc3d98326231c
KayongoYongo <sknsammie10@gmail.com>
2023-07-04 17:48:27 +0000

    updated fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,6 @@
-/*This function returns an array of ids in a function
- *
- *
- *
- *
- *return: array of ids
- */
 export default function getListStudentIds(array) {
   if (Array.isArray(array)) {
-    return array.map(item => item.id);;
+    return array.map(item => item.id);
   } else {
     return [];
   }
