6ad968486deb9e5a9c52213ac8d39f6360df584e
KayongoYongo <sknsammie10@gmail.com>
2023-07-04 19:20:28 +0000

    error in end point

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+export { weakMap };
+
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+  
+  if (count >= 4) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
