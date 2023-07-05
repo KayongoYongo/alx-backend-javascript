724194ddffe16f4d7c043e0e36f40b57ccc92ecf
KayongoYongo <sknsammie10@gmail.com>
2023-07-05 16:12:24 +0000

    task 1

---
+++ 0x04-TypeScript/task_1/main.ts
@@ -0,0 +1,19 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly fullTimeEmployee: boolean;
+  readonly yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
