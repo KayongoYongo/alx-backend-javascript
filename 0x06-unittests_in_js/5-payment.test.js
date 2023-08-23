d7d9c56a6938a37318ebc60283ab2acee0d30d86
KayongoYongo <sknsammie10@gmail.com>
2023-08-24 01:59:00 +0300

    hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,38 +1,23 @@
-const sinon = require('sinon');
-const expect = require('chai').expect;
-const sendPaymentRequestToApi = require('./5-payment.js');
-const Utils = require('./utils');
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./5-payment");
+const expect = require("chai").expect;
 
-describe('sendPaymentRequestToApi', function () {
-  let calculateNumberStub;
-  let consoleSpy;
+describe("sendPaymentRequestToApi", function() {
+    beforeEach("Set up spy to use for each test", function() {
+	sinon.spy(console, "log");
+    });
+    afterEach("restore spy after each test", function() {
+	console.log.restore();
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(100, 20);
 
-  beforeEach(function () {
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    consoleSpy = sinon.spy(console, 'log');
-  });
+	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(10, 10);
 
-  afterEach(function () {
-    calculateNumberStub.restore();
-    consoleSpy.restore();
-  });
-
-  it('should log the correct message for the first scenario', function () {
-    sendPaymentRequestToApi(100, 20);
-
-    expect(consoleSpy.calledOnceWith('The total is: 120')).to.be.true;
-  });
-
-  it('should log the correct message for the second scenario', function () {
-    sendPaymentRequestToApi(10, 10);
-
-    expect(consoleSpy.calledOnceWith('The total is: 20')).to.be.true;
-  });
-
-  it('should only call console.log once for each scenario', function () {
-    sendPaymentRequestToApi(100, 20);
-    sendPaymentRequestToApi(10, 10);
-
-    expect(consoleSpy.callCount).to.equal(2);
-  });
+	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
+    });
 });
