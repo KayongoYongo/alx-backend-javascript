701d07aa11a34f744a90dde262664fec225ffbc8
KayongoYongo <sknsammie10@gmail.com>
2023-08-24 01:17:01 +0300

    update

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,33 +1,33 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul.js');
+const expect = require('chai').expect;
+const calculateNumber = require('./2-calcul_chai.js');
 
 describe('calculateNumber', function () {
   describe('type SUM', function () {
     it('should return the sum of rounded numbers', function () {
-      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+      expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
     });
   });
 
   describe('type SUBTRACT', function () {
     it('should return the difference of rounded numbers', function () {
-      assert.strictEqual(calculateNumber('SUBTRACT', 5, 2), 3);
-      assert.strictEqual(calculateNumber('SUBTRACT', 2, 5), -3);
+      expect(calculateNumber('SUBTRACT', 5, 2)).to.equal(3);
+      expect(calculateNumber('SUBTRACT', 2, 5)).to.equal(-3);
     });
   });
 
   describe('type DIVIDE', function () {
     it('should return the division of rounded numbers', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 6, 2), 3);
-      assert.strictEqual(calculateNumber('DIVIDE', 6, 4), 1.5);
+      expect(calculateNumber('DIVIDE', 6, 2)).to.equal(3);
+      expect(calculateNumber('DIVIDE', 6, 4)).to.equal(1.5);
     });
 
     it('should return "Error" when attempting to divide by 0', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 6, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 6, 0)).to.equal('Error');
     });
   });
 
   it('should throw an error for invalid type', function () {
-    assert.throws(() => calculateNumber('INVALID_TYPE', 1, 2), Error);
+    expect(() => calculateNumber('INVALID_TYPE', 1, 2)).to.throw(Error);
   });
 });
