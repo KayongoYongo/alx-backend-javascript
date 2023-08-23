11c429817c473ad09c568a1571769382036ac4d0
KayongoYongo <sknsammie10@gmail.com>
2023-08-24 01:36:12 +0300

    spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with SUM and the provided values', function () {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledOnce).to.be.true;
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+});
