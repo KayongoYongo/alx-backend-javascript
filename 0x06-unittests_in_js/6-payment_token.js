1cc21df79ed48a0a4c2edd953709a279a3a43c79
KayongoYongo <sknsammie10@gmail.com>
2023-08-25 00:54:41 +0300

    update

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,10 +1,9 @@
 function getPaymentTokenFromAPI(success) {
-    if (success) {
-      return Promise.resolve({ data: 'Successful response from the API' });
-    } else {
-      return Promise.resolve();
-    }
+  if (success) {
+    return new Promise((resolve) => {
+        resolve({data: 'Successful response from the API' });
+    });
   }
-  
-module.exports = { getPaymentTokenFromAPI };
-  
+}
+
+module.exports = getPaymentTokenFromAPI;
